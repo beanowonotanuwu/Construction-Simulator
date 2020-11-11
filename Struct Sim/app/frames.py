f1e7ae132936c78f82ee13cbc564365ebc8d4c7a
Hunter Garza <Garza_888516@leanderisd.org>
2020-11-11 12:40:52 -0600

    Added a Main Frame and a dependency, easyyaml

--- Struct Sim/app/frames.py
+++ Struct Sim/app/frames.py
@@ -1,5 +1,19 @@
 ### IMPORTS                         ###
 from tkinter import ttk     ## ttk > tk; tk is not maintained in python 3
+from tkinter import (
+    Frame
+)
 ### IMPORTS                         ###
 ### FRAMES                          ###
+class Main(Frame):
+    def __init__(self, parent, controller):
+        ## Init                         ##
+        Frame.__init__(self, parent)
+        ## Init                         ##
+        ## Buttons                      ##
+        sel_model = ttk.Button(
+            self,
+            text='Select Model'
+        )
+        ## Buttons                      ##
 ### FRAMES                          ###
