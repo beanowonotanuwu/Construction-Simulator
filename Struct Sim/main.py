484ff34632fccbf7a9da6ddf9a9929a21673b334
Hunter Garza <Garza_888516@leanderisd.org>
2020-11-11 11:34:52 -0600

    Added base Python file (main.py and respective directory)

---
+++ Struct Sim/main.py
@@ -0,0 +1,5 @@
+### IMPORTS                         ###
+### IMPORTS                         ###
+### __MAIN__                        ###
+if __name__ == "__main__": pass
+### __MAIN__                        ###
