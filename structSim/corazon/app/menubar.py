c44e169283b23f323d5ca2c13cd0ced92378a041
Hunter Garza <Garza_888516@leanderisd.org>
2020-11-13 08:08:55 -0600

    Added viewmenu and simulate to menubar

--- structSim/corazon/app/menubar.py
+++ structSim/corazon/app/menubar.py
@@ -10,14 +10,25 @@ class Menubar(Menu):
     def __init__(self, master, *args, **kwargs):
         ## Init                         ##
         super().__init__(master=master, *args, **kwargs)
+        self.master = master
         ## Init                         ##
         ## Menus                        ##
-        construct_menu = Construct(self, tearoff=0)
+        construct_menu      = Construct(self, tearoff=0)
+        view_menu           = ViewMenu(self, tearoff=0)
+        simulate_menu       = Simulate(self, tearoff=0)
         ## Menus                        ##
         ## Cascades                     ##
         self.add_cascade(
             label="Construct",
             menu=construct_menu
+        )
+        self.add_cascade(
+            label="View",
+            menu=view_menu
+        )
+        self.add_cascade(
+            label="Simulate",
+            menu=simulate_menu
         )
         ## Cascades                     ##
 ### MENU                            ###
@@ -35,3 +46,29 @@ class Construct(Menu):
         self.add_command(label="Close Application", command=master.quit)
         ## Commands                     ##
 ### CONSTRUCT MENU                  ###
+### VIEW MENU                       ###
+class ViewMenu(Menu):
+    def __init__(self, master, *args, **kwargs):
+        ## Init                         ##
+        super().__init__(master=master, *args, **kwargs)
+        ## Init                         ##            
+        ## Commands                     ##
+        self.add_command(label="Fullscreen")
+        ## Commands                     ##
+### VIEW MENU                       ###
+### SIMULATE MENU                   ###
+class Simulate(Menu):
+    def __init__(self, master, *args, **kwargs):
+        ## Init                         ##
+        super().__init__(master=master, *args, **kwargs)
+        ## Init                         ##
+        ## Commands                     ##
+        self.add_command(label="Start with Statistics")
+        self.add_command(label="Start without Statistics")
+        self.add_command(label="Restart with Statistics")
+        self.add_command(label="Restart without Statistics")
+        self.add_command(label="Pause...")
+        self.add_separator()
+        self.add_command(label="Stop Simulation")
+        ## Commands                     ##
+### SIMULATE MENU                   ###
